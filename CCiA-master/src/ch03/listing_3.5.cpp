58a8f513f23d49305af05edb256ebc2a4b5fddd0
Hyunjin <roykim0823@gmail.com>
2018-10-17 16:54:20 -0700

    10172018

--- CCiA-master/src/ch03/listing_3.5.cpp
+++ CCiA-master/src/ch03/listing_3.5.cpp
@@ -2,8 +2,11 @@
 #include <stack>
 #include <mutex>
 #include <memory>
+#include <iostream>
 
-struct empty_stack: std::exception
+// default type of inheritance to class is "private"
+// default type of inheritance to struct is "public"
+struct empty_stack: std::exception	//	public inheritance
 {
     const char* what() const throw()
     {
@@ -17,12 +20,12 @@ class threadsafe_stack
 {
 private:
     std::stack<T> data;
-    mutable std::mutex m;
+    mutable std::mutex m;	// mutable for bool empty() const
 public:
     threadsafe_stack(){}
     threadsafe_stack(const threadsafe_stack& other)
     {
-        std::lock_guard<std::mutex> lock(other.m);
+        std::lock_guard<std::mutex> lock(other.m);	// lock the source object mutex
         data=other.data;
     }
     threadsafe_stack& operator=(const threadsafe_stack&) = delete;
@@ -57,12 +60,15 @@ public:
 int main()
 {
     threadsafe_stack<int> si;
-    si.push(5);
-    si.pop();
-    if(!si.empty())
+	for(int i=0; i<10; i++) 
+		si.push(i);
+    while(!si.empty())
     {
         int x;
-        si.pop(x);
+        si.pop(x);			// void pop(T& value)
+		std::cout << x << std::endl;
+        auto y = si.pop();  // std::shared_ptr<T> pop()
+		std::cout << *y << std::endl;
     }
     
 }
