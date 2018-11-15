6ec628fa33efa10d8770e32ab59e84063d91c99d
Hyunjin <roykim0823@gmail.com>
2018-11-14 18:08:17 -0800

    11142018

--- Template/basics/src/errornovel1.cpp
+++ Template/basics/src/errornovel1.cpp
@@ -8,7 +8,11 @@ int main()
   //...
   // find the first nonempty string in coll:
   auto pos = std::find_if (coll.begin(), coll.end(),
-                           [] (std::string const& s) { 
-                             return s != "";
+                           //[] (std::string const& s) { 
+                           [] (const std::map<std::string,double>::value_type & s) { 	// fixed
+                           //[] (const std::pair<std::string,double>& s) { 
+                           //[] (std::pair<std::string,double>& s) { 
+                           //  return s != "";
+						   		return s.first != "";
                            });
 }
