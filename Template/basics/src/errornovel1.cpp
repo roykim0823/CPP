8b5789cbfd0dac4f374f3c6f314b9befa164b51b
Hyunjin Kim <roykim0823@gmail.com>
2018-11-22 12:17:47 -0800

    11222018

--- Template/basics/src/errornovel1.cpp
+++ Template/basics/src/errornovel1.cpp
@@ -4,14 +4,14 @@
 
 int main()
 {
-  std::map<std::string,double> coll;                  
+  std::map<std::string,double> coll;
   //...
   // find the first nonempty string in coll:
   auto pos = std::find_if (coll.begin(), coll.end(),
-                           //[] (std::string const& s) { 
-                           [] (const std::map<std::string,double>::value_type & s) { 	// fixed
-                           //[] (const std::pair<std::string,double>& s) { 
-                           //[] (std::pair<std::string,double>& s) { 
+                           //[] (std::string const& s) {  // errror
+                           [] (const std::map<std::string,double>::value_type & s) { 	// fix #1
+                           //[] (const std::pair<std::string,double>& s) {  // fix #2
+                           //[] (std::pair<std::string,double>& s) {        // fix #3
                            //  return s != "";
 						   		return s.first != "";
                            });
