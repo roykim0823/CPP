6b8ac6ac797bf434d2e1fcf9f5c77f0396ac8cc4
Hyunjin <roykim0823@gmail.com>
2019-02-07 16:09:45 -0800

    02072019

---
+++ DesignPattern/DPModern/Structural/Bridge/include/Renderer.h
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+// two classes of objects
+
+// Renderers - determine how an object is drawn
+// Shapes - determine what to draw
+
+struct Renderer
+{
+  virtual void render_circle(float x, float y, float radius) = 0;
+};
+
+struct VectorRenderer : Renderer
+{
+  void render_circle(float x, float y, float radius) override
+  {
+    cout << "Rasterizing circle of radius " << radius << endl;
+  }
+};
+
+struct RasterRenderer : Renderer
+{
+  void render_circle(float x, float y, float radius) override
+  {
+    cout << "Drawing a vector circle of radius " << radius << endl;
+  }
+};
+
---
+++ DesignPattern/DPModern/Structural/Bridge/include/Shape.h
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include "Renderer.h"
+using namespace std;
+
+// two classes of objects
+
+// Renderers - determine how an object is drawn
+// Shapes - determine what to draw
+
+struct Shape
+{
+protected:
+  Renderer& renderer;
+  Shape(Renderer& renderer) : renderer{ renderer } {}
+public: virtual void draw() = 0; // implementation specific
+  virtual void resize(float factor) = 0; // abstraction specific
+};
+
+struct Circle : Shape
+{
+  float x, y, radius;
+
+  void draw() override
+  {
+    renderer.render_circle(x, y, radius);
+  }
+
+  void resize(float factor) override
+  {
+    radius *= factor;
+  }
+
+  Circle(Renderer& renderer, float x, float y, float radius)
+    : Shape{renderer},
+      x{x},
+      y{y},
+      radius{radius}
+  {
+  }
+};
+
