e1d65b062844b2c3cc8350e8ea53a60162ae447c
Hyunjin Kim <roykim0823@gmail.com>
2019-04-04 15:59:20 -0700

    04042019

---
+++ DesignPattern/DPModern/Behavioral/Command/src/cqs.cpp
@@ -0,0 +1,19 @@
+﻿#include "cqs.h"
+#include <iostream>
+#include <algorithm> 	// for for_each
+
+using namespace std;
+
+int main() {
+	Creature c(100, 200);
+	int strength, agility;
+
+	cout << "Init" << endl;
+	cout << "strength = " << c.get_strength() << ", agility = " << c.get_agility() << endl;
+    c.set_strength(111);
+    c.set_agility(222);
+	cout << "Set" << endl;
+	cout << "strength = " << c.get_strength() << ", agility = " << c.get_agility() << endl;
+
+	return 0;
+}
---
+++ DesignPattern/DPModern/Behavioral/Interpreter/include/lex.h
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using std::vector;
+using std::string;
+using std::ostream;
+
+struct Token {
+  	enum Type { integer, plus, minus, lparen, rparen } type;
+  	string text;
+
+ 	explicit Token(Type type, const string& text)
+    : type{type}, text{text} {}
+
+  	friend ostream& operator<<(ostream& os, const Token& obj) {
+    	return os << "`" << obj.text << "`";
+  	}
+};
+
+vector<Token> lex(const string& input) {
+	vector<Token> result;
+
+  	for (int i = 0; i < input.size(); ++i) {
+    	switch (input[i]) {
+    	case '+':
+      		result.push_back(Token{ Token::plus, "+" });
+      		break;
+    	case '-':
+      		result.push_back(Token{ Token::minus, "-" });
+      		break;
+    	case '(':
+      		result.push_back(Token{ Token::lparen, "(" });
+      		break;
+    	case ')':
+      		result.push_back(Token{ Token::rparen, ")" });
+      		break;
+    	default:
+      		// number
+      		ostringstream buffer;
+      		buffer << input[i];
+      		for (int j = i + 1; j < input.size(); ++j) {
+        		if (isdigit(input[j])) {
+          			buffer << input[j];
+          			++i;
+        		} else {
+          			result.push_back(Token{ Token::integer, buffer.str() });
+          			break;
+        		}
+      		}
+    	}
+  	}
+  	return result;
+}
