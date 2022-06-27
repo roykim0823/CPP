6856f2982b71980d6494f523255f10feda72bed4
hyunjin.kim <roykim0823@gmail.com>
2022-06-27 18:14:10 +0900

    Add TestDriven/2*
    Signed-off-by: hyunjin.kim <roykim0823@gmail.com>

---
+++ TestDriven/21_GoogleTestAAA/GoogleTestAAA/testRunner.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <gtest/gtest.h>
+#include "LibraryCode.hpp"
+
+TEST(TestCountPositives, BasicTest)
+{
+    //Arrange
+    std::vector<int> inputVector{1, -2, 3, -4, 5, -6, -7};
+
+    //Act
+    int count = countPositives(inputVector);
+
+    //Assert
+    ASSERT_EQ(3, count);
+}
+
+TEST(TestCountPositives, EmptyVectorTest)
+{
+    //Arrange
+    std::vector<int> inputVector{};
+
+    //Act
+    int count = countPositives(inputVector);
+
+    //Assert
+    ASSERT_EQ(0, count);
+}
+
+TEST(TestCountPositives, AllNegativesTest)
+{
+
+    //Arrange
+    std::vector<int> inputVector{-1, -2, -3};
+
+    //Act
+    int count = countPositives(inputVector);
+
+    //Assert
+    ASSERT_EQ(0, count);
+}
+
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
