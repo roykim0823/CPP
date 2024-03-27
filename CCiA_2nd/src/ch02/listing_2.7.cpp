af014f329b11f49fac5c05df2b54e4549bfe89d1
roy <roykim0823@gmail.com>
2024-03-27 16:06:55 +0900

    update ch2 and add 2.7
    Signed-off-by: roy <roykim0823@gmail.com>

--- CCiA_2nd/src/ch02/listing_2.7.cpp
+++ CCiA_2nd/src/ch02/listing_2.7.cpp
@@ -1,20 +1,92 @@
-#include <vector>
 #include <thread>
-#include <algorithm>
-#include <functional>
+#include <utility>
 
-void do_work(unsigned id)
+class joining_thread {
+    std::thread t;
+public:
+    joining_thread() noexcept=default;
+    template<typename Callable, typename ... Args>
+    explicit joining_thread(Callable&& func, Args&& ... args):
+        t(std::forward<Callable>(func), std::forward<Args>(args)...)
+    {}
+
+    explicit joining_thread(std::thread t_) noexcept:
+        t(std::move(t_))
+    {}
+
+    joining_thread(joining_thread&& other) noexcept:
+        t(std::move(other.t))
+    {}
+    
+    joining_thread& operator=(joining_thread&& other) noexcept {
+        if(joinable())
+            join();
+        t=std::move(other.t);
+        return *this;
+    }
+
+    joining_thread& operator=(std::thread other) noexcept {
+        if(joinable())
+            join();
+        t=std::move(other);
+        return *this;
+    }
+    ~joining_thread() noexcept {
+        if(joinable())
+            join();
+    }
+    void swap(joining_thread& other) noexcept {
+        t.swap(other.t);
+    }
+    std::thread::id get_id() const noexcept {
+        return t.get_id();
+    }
+    bool joinable() const noexcept {
+        return t.joinable();
+    }
+    void join() {
+        t.join();
+    }
+    void detach() {
+        t.detach();
+    }
+    std::thread& as_thread() noexcept {
+        return t;
+    }
+    const std::thread& as_thread() const noexcept {
+        return t;
+    }
+};
+
+void do_something(int& i)
+{
+    ++i;
+}
+
+struct func
+{
+    int& i;
+
+    func(int& i_):i(i_){}
+
+    void operator()()
+    {
+        for(unsigned j=0;j<1000000;++j)
+        {
+            do_something(i);
+        }
+    }
+};
+
+void do_something_in_current_thread()
 {}
 
 void f()
 {
-    std::vector<std::thread> threads;
-    for(unsigned i=0;i<20;++i)
-    {
-        threads.push_back(std::thread(do_work,i));
-    }
-    std::for_each(threads.begin(),threads.end(),
-        std::mem_fn(&std::thread::join));
+    int some_local_state;
+    joining_thread t({std::thread(func(some_local_state))});
+        
+    do_something_in_current_thread();
 }
 
 int main()
