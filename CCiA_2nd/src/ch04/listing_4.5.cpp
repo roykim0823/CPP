eed9866037f966a1176b20781bf4c774cb159d78
hyunjin <roykim0823@gmail.com>
2024-11-03 04:45:45 +0900

    CCiA 2nd ch4
    Signed-off-by: hyunjin <roykim0823@gmail.com>

--- CCiA_2nd/src/ch04/listing_4.5.cpp
+++ CCiA_2nd/src/ch04/listing_4.5.cpp
@@ -1,71 +1,7 @@
-#include <mutex>
-#include <condition_variable>
+#include "include/thread_safe_queue.h"
+
 #include <iostream>
-#include <queue>
-#include <memory>
 #include <thread>
-
-template<typename T>
-class threadsafe_queue
-{
-private:
-    mutable std::mutex mut;
-    std::queue<T> data_queue;
-    std::condition_variable data_cond;
-public:
-    threadsafe_queue() {}
-    threadsafe_queue(threadsafe_queue const& other) {
-        std::lock_guard<std::mutex> lk(other.mut);
-        data_queue=other.data_queue;
-    }
-
-    void push(T new_value) {
-        std::lock_guard<std::mutex> lk(mut);
-        std::cout << "push(" << new_value << ")" << std::endl;
-        data_queue.push(new_value);
-        data_cond.notify_one();
-    }
-
-    void wait_and_pop(T& value) {
-        std::unique_lock<std::mutex> lk(mut);
-        data_cond.wait(lk,[this]{return !data_queue.empty();});
-        value=data_queue.front();
-        data_queue.pop();
-        std::cout << "void wait_and_pop(" << value << ")" << std::endl;
-    }
-
-    std::shared_ptr<T> wait_and_pop() {
-        std::unique_lock<std::mutex> lk(mut);
-        data_cond.wait(lk,[this]{return !data_queue.empty();});
-        std::shared_ptr<T> res(std::make_shared<T>(data_queue.front()));
-        data_queue.pop();
-        std::cout << "std::shared_ptr<T> wait_and_pop(" << *res << ")" << std::endl;
-        return res;
-    }
-
-    bool try_pop(T& value) {
-        std::lock_guard<std::mutex> lk(mut);
-        if(data_queue.empty())
-            return false;
-        value=data_queue.front();
-        data_queue.pop();
-        return true;
-    }
-
-    std::shared_ptr<T> try_pop() {
-        std::lock_guard<std::mutex> lk(mut);
-        if(data_queue.empty())
-            return std::shared_ptr<T>();
-        std::shared_ptr<T> res(std::make_shared<T>(data_queue.front()));
-        data_queue.pop();
-        return res;
-    }
-
-    bool empty() const {
-        std::lock_guard<std::mutex> lk(mut);
-        return data_queue.empty();
-    }
-};
 
 void producer(threadsafe_queue<int>& ts_q) {
     ts_q.push(1);
